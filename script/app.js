c6375ae8f0c6d1ccddb31294536ab2f508e319de
imrish-18 <rish.sharma9012@gmail.com>
2020-06-18 16:54:05 +0530

    commit

--- script/app.js
+++ script/app.js
@@ -1,30 +1,19 @@
-// console.log('starting...');
-// // msg will print after the time given to setTimeout()
-// setTimeout(()=>{
-// console.log('2 sec timer...')
-// },2000)
 
-// setTimeout(()=>{
-//     console.log('0 sec timer...')
-//     },0)
-    
-
-// for(let i=0;i<10;i++)
-// console.log(Math.random());
-// console.log('stoping....')
-
-const req=require('request');
-const request = require('request');
-const url=
-'http://api.weatherstack.com/current?access_key=6e1c3a3bdc20126dd2f144618e44b7c0&query=bijnor'
-request({ url: url, json: true }, (error, response) => {
-    console.log(
-        ' It is currently ' + response.body.current.temperature + 
-        ' degress out. There is a ' + response.body.current.precip 
-        + '% chance of rain.')
+// use call back function
+const request = require('request')
+const chalk = require('chalk')
+const geocode = require('./geocode')
+const forecast=require('./forecast')
+// process.argv is used to take input from user
+console.log(process.argv)
+const address=process.argv[2]
+//using location
+geocode(address, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
-
-// TOKEN FOR WEATHER STACK .COM 6e1c3a3bdc20126dd2f144618e44b7c0
-// TOKEN FOR MAPBOX pk.eyJ1IjoicmlzaHNoYXJtYTkwMTIiLCJhIjoiY2tiazdzeG9uMDBlNDJ1cXQ5eXRwNnFsbiJ9.sw50PE8rcRUxNSqqVJEYgw
-Request is designed to be the simplest way possible to make http calls. It supports HTTPS and follows redirects by default.
-
+// using longitude and latitude
+forecast(29.2523,78.6881, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
